bcdb0f3161c8ab06e9fd993731af13ef326a8341
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2020-10-04 14:33:56 -0400

    Complete repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,4 +1,20 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+    let repeatedString = '';
+
+    // loop set by the input of the parameter
+    while (times > 0){
+        repeatedString = repeatedString + str;
+        times--;
+
+        
+    }
+
+    if(times < 0){
+        repeatedString = 'ERROR';
+    }
+    
+    return repeatedString;
+
 
 }
 
