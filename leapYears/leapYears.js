8bc948bcc7f80183a92617b938ce4479d9ffdba0
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2020-10-06 20:03:00 -0400

    Completed leapyears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    // What is a century year? It's a year that is divisble by 100
+    // If it's a century year, the year must then be divisble by 400 to be a leap year
+    // let centuryYear = (year % 100 === 0);
+
+    if((year % 100 === 0 && year % 400 === 0) || (year % 100 != 0 && year % 4 === 0)){
+        return true;
+    } else {
+        return false;
+    }
 
 }
 
