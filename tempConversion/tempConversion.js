219e03d42d84298a3d203b87dfb1b18c834ac17f
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2020-10-06 20:18:42 -0400

    Completed tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,8 +1,30 @@
-const ftoc = function() {
+const ftoc = function(tempF) {
 
+  let celNum = (tempF - 32) * (5/9);
+
+  if(tempF == 32){
+    return 0;
+  } else {
+    let celsius = Math.round(celNum * 10) / 10;
+    return celsius;
+  }
+  
 }
 
-const ctof = function() {
+const ctof = function(tempC) {
+
+  let fahNum = (tempC * 9/5) + 32;
+
+  if(tempC == 0){
+    return 32;
+  } else {
+    let fahrenheit = Math.round(fahNum * 10) / 10;
+    return fahrenheit;
+  }
+  
+
+  
+  
 
 }
 
