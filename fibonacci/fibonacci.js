a180d8191bc81913dfc53b9e59325f8cc8d2b7d8
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2020-11-08 21:02:58 -0500

    Updates

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,25 +1,26 @@
-const fibonacci = function(num) {
-  // a series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc.
-  
-  // If they input function(3) then they should be able to receive
-  // the sum of all the numebers up to that point in the array
-  
-  // Let's create the alg to sum the numbers
-  
-  arr = [1, 1, 2, 3, 5, 8];
-  
-  // We can use a reduce function, but depending on the number inputted. So we need to first create a new array based on the input using slice
-  
-  let arrFib = arr.slice(0, num+1);
+const fibonacci = function (num) {
+  // a series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc. 
 
-  // Now we can use reduce
-  let sum = arrFib.reduce(function(total, arrNum) {
-                          return total + arrNum;
-                          }, 0);
-  
-  console.log(sum);
-  
-  // now we can create the fibonacci sequence
+  let arr = [1];
+
+  let arrFib;
+
+
+  // We have the general algorith, but we need to use a loop to append to arr instead of using an existing array
+
+  for (i = 0; i <= num; i++) {
+    arrFib = arr.slice(0, i);
+
+    // Now we can use reduce
+    let sum = arrFib.reduce(function (total, arrNum) {
+      return total + arrNum;
+    }, 0);
+
+    console.log(sum);
+    arrFib.push(sum);
+  }
+  console.log(arrFib);
+
 }
 
 // module.exports = fibonacci
