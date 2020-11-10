cfd25cb1d8007c2f14b42ce6ce2120790f119129
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2020-11-09 20:26:15 -0500

    Done

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,19 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+    // Figured out how to access the objects
+
+    // We need to access the first and second index of books to get the title and add them to an array
+
+    let booksArr = [];
+    let currentBook;
+
+    for(i=0; i < books.length; i++){
+        currentBook = books[i].title;
+
+        booksArr.push(currentBook);
+    }
+
+    return booksArr;
 }
 
 module.exports = getTheTitles;
